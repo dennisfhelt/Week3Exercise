6c1e65cc94432213da83e5c99871bc7ceaea734e
Dennis Helt <dfhelt@madisoncollege.edu>
2018-02-26 14:20:13 -0600

    Modifying

--- IdeaProjects/week-4-exercise-hibernate-dennisfhelt/src/main/java/edu/matc/entity/Book.java
+++ IdeaProjects/week-4-exercise-hibernate-dennisfhelt/src/main/java/edu/matc/entity/Book.java
@@ -1,5 +1,10 @@
 package edu.matc.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+
 /**
  * TODO At minimum, complete the following
  * TODO create no-arg constructor
@@ -7,10 +12,132 @@ package edu.matc.entity;
  * TODO add hibernate annotations
  * TODO Javadoc comments
  */
+@Entity(name="Book")
+@Table(name="book")
+
+
 public class Book {
     private String title;
     private String author;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
+    @GenericGenerator(name="native", strategy="native")
     private int id;
     private String isbn;
-    private int publicationYear;
+
+    @Column(name="publication_year")
+    private String publicationYear;
 }
+
+
+
+    public Book() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param userName  the user name
+     * @param id        the id
+     * @param dateOfBirth  the date of birth
+     */
+    public User(String firstName, String lastName, String userName, int id, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Sets date of birth.
+     *
+     * @param dateOfBirth the date of birth
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * Gets date of birth.
+     *
+     * @return the date of birth
+     */
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
